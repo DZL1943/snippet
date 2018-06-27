fe790d94398ea8a7e8aa204e2d9a9ce0ad776331
6Bd2 <ldz1943@gmail.com>
2018-06-27 11:17:40 +0800

    Update ch5.sh

--- os/linux/LFS/ch5.sh
+++ os/linux/LFS/ch5.sh
@@ -48,8 +48,33 @@ sed -i '/k prot/agcc_cv_libc_provides_ssp=yes' gcc/configure
 
 mkdir -v ../gcc-build
 pushd ../gcc-build
+../gcc-4.9.2/configure                             \
+    --target=$LFS_TGT                              \
+    --prefix=/tools                                \
+    --with-sysroot=$LFS                            \
+    --with-newlib                                  \
+    --without-headers                              \
+    --with-local-prefix=/tools                     \
+    --with-native-system-header-dir=/tools/include \
+    --disable-nls                                  \
+    --disable-shared                               \
+    --disable-multilib                             \
+    --disable-decimal-float                        \
+    --disable-threads                              \
+    --disable-libatomic                            \
+    --disable-libgomp                              \
+    --disable-libitm                               \
+    --disable-libquadmath                          \
+    --disable-libsanitizer                         \
+    --disable-libssp                               \
+    --disable-libvtv                               \
+    --disable-libcilkrts                           \
+    --disable-libstdc++-v3                         \
+    --enable-languages=c,c++
 make
 make install
 popd
 popd
+
+
 popd
