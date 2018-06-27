687e66a36e511a349873d25c54dba5d5fdd21265
6Bd2 <ldz1943@gmail.com>
2018-06-27 12:56:34 +0800

    Update ch5.sh

--- os/linux/LFS/ch5.sh
+++ os/linux/LFS/ch5.sh
@@ -20,7 +20,7 @@ make install
 popd
 popd
 
-# 2 gcc
+# 2. gcc
 tar -xjvf gcc-4.9.2.tar.bz2
 pushd gcc-4.9.2
 tar -xf ../mpfr-3.1.2.tar.xz
@@ -46,7 +46,7 @@ done
 
 sed -i '/k prot/agcc_cv_libc_provides_ssp=yes' gcc/configure
 
-mkdir -v ../gcc-build
+mkdir -pv ../gcc-build
 pushd ../gcc-build
 ../gcc-4.9.2/configure                             \
     --target=$LFS_TGT                              \
@@ -76,5 +76,44 @@ make install
 popd
 popd
 
+# 3. linux-headers
+tar -xJvf linux-3.19.tar.xz
+pushd linux-3.19
+make mrproper
+make INSTALL_HDR_PATH=dest headers_install
+cp -rv dest/include/* /tools/include
+popd
+
+# 4. glibc
+tar -xJvf glibc-2.21.tar.xz
+pushd glibc-2.21
+if [ ! -r /usr/include/rpc/types.h ]; then
+  su -c 'mkdir -pv /usr/include/rpc'
+  su -c 'cp -v sunrpc/rpc/*.h /usr/include/rpc'
+fi
+sed -e '/ia32/s/^/1:/' \
+    -e '/SSE2/s/^1://' \
+    -i  sysdeps/i386/i686/multiarch/mempcpy_chk.S
+mkdir -pv ../glibc-build
+pushd ../glibc-build
+../glibc-2.21/configure                             \
+      --prefix=/tools                               \
+      --host=$LFS_TGT                               \
+      --build=$(../glibc-2.21/scripts/config.guess) \
+      --disable-profile                             \
+      --enable-kernel=2.6.32                        \
+      --with-headers=/tools/include                 \
+      libc_cv_forced_unwind=yes                     \
+      libc_cv_ctors_header=yes                      \
+      libc_cv_c_cleanup=yes
+make
+make install
+
+echo 'main(){}' > dummy.c
+$LFS_TGT-gcc dummy.c
+readelf -l a.out | grep ': /tools'
+rm -v dummy.c a.out
+popd
+popd
 
 popd
