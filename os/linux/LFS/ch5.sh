23cc3cef2c3a4d870ebf1b384ddef047c23b0cab
6Bd2 <ldz1943@gmail.com>
2018-06-27 13:27:11 +0800

    Update ch5.sh

--- os/linux/LFS/ch5.sh
+++ os/linux/LFS/ch5.sh
@@ -117,7 +117,9 @@ popd
 popd
 
 # 5. libstdc++
-pushd gcc-build
+# make a new dir
+mkdir -pv libstdcpp-build
+pushd libstdcpp-build
 ../gcc-4.9.2/libstdc++-v3/configure \
     --host=$LFS_TGT                 \
     --prefix=/tools                 \
@@ -131,5 +133,23 @@ make
 make install
 popd
 
+# 6. binutils-2
+mkdir -pv binutils-build2
+pushd binutils-build2
+CC=$LFS_TGT-gcc                \
+AR=$LFS_TGT-ar                 \
+RANLIB=$LFS_TGT-ranlib         \
+../binutils-2.25/configure     \
+    --prefix=/tools            \
+    --disable-nls              \
+    --disable-werror           \
+    --with-lib-path=/tools/lib \
+    --with-sysroot
+make
+make install
+make -C ld clean
+make -C ld LIB_PATH=/usr/lib:/lib
+cp -v ld/ld-new /tools/bin
+popd
 
 popd
